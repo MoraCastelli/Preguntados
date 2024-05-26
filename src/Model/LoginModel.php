b009f4927de9703dcaf9ba8efb207e7e440c4fb0
Lucía Alarcón <lalarcon782@alumno.unlam.edu.ar>
2024-05-26 20:02:12 -0300

    cambio

--- src/Model/LoginModel.php
+++ src/Model/LoginModel.php
@@ -25,7 +25,7 @@ class LoginModel
 public function LogInconsulta($usuario, $password)
 {
 
-    $sql = "SELECT * FROM usuarios WHERE nombre_usuario = 'admin' AND password = 'admin'";
+    $sql = "SELECT * FROM usuario WHERE nombre_usuario = 'admin' AND password = 'admin'";
     $result = $this->Database->query($sql);
     //return $result;
     return false;
